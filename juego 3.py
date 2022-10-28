d66228753794ca9745eccf3b64fc41068fa1aa59
LeonCamandona <camandonaleon@gmail.com>
2022-10-28 11:24:02 -0300

    actualizacion

---
+++ juego 3.py
@@ -0,0 +1,240 @@
+import random
+menu=3
+if menu == 3:
+        element = random.randint(1, 118)
+        if element == 1:
+            print("hidrogeno")
+        if element == 2:
+            print("helio")
+        if element == 3:
+            print("litio")
+        if element == 4:
+            print("berilio")
+        if element == 5:
+            print("boro")
+        if element == 6:
+            print("carbono")
+        if element == 7:
+            print("nitrogeno")
+        if element == 8:
+            print("oxigeno")
+        if element == 9:
+            print("fluor")
+        if element == 10:
+            print("neon")
+        if element == 11:
+            print("sodio")
+        if element == 12:
+            print("magnesio")
+        if element == 13:
+            print("aluminio")
+        if element == 14:
+            print("silicio")
+        if element == 15:
+            print("fosforo")
+        if element == 16:
+            print("azufre")
+        if element == 17:
+            print("cloro")
+        if element == 18:
+            print("argon")
+        if element == 19:
+            print("potasio")
+        if element == 20:
+            print("calcio")
+        if element == 21:
+            print("escandio")
+        if element == 22:
+            print("titanio")
+        if element == 23:
+            print("vanadio")
+        if element == 24:
+            print("cromo")
+        if element == 25:
+            print("manganeso")
+        if element == 26:
+            print("hierro")
+        if element == 27:
+            print("cobalto")
+        if element == 28:
+            print("niquel")
+        if element == 29:
+            print("cobre")
+        if element == 30:
+            print("zinc")
+        if element == 31:
+            print("galio")
+        if element == 32:
+            print("germanio")
+        if element == 33:
+            print("arsenico")
+        if element == 34:
+            print("selenio")
+        if element == 35:
+            print("bromo")
+        if element == 36:
+            print("kripton")
+        if element == 37:
+            print("rubidio")
+        if element == 38:
+            print("estroncio")
+        if element == 39:
+            print("itrio")
+        if element == 40:
+            print("circonio")
+        if element == 41:
+            print("niobio")
+        if element == 42:
+            print("molibdeno")
+        if element == 43:
+            print("tecnecio")
+        if element == 44:
+            print("rutenio")
+        if element == 45:
+            print("rodio")
+        if element == 46:
+            print("paladio")
+        if element == 47:
+            print("plata")
+        if element == 48:
+            print("cadmio")
+        if element == 49:
+            print("indio")
+        if element == 50:
+            print("estaño")
+        if element == 51:
+            print("antimonio")
+        if element == 52:
+            print("telurio")
+        if element == 53:
+            print("yodo")
+        if element == 54:
+            print("xenon")
+        if element == 55:
+            print("cesio")
+        if element == 56:
+            print("bario")
+        if element == 57:
+            print("lantano")
+        if element == 58:
+            print("cerio")
+        if element == 59:
+            print("praseodimio")
+        if element == 60:
+            print("neodimio")
+        if element == 61:
+            print("prometio")
+        if element == 62:
+            print("samario")
+        if element == 63:
+            print("europio")
+        if element == 64:
+            print("gadolinio")
+        if element == 65:
+            print("terbio")
+        if element == 66:
+            print("disprosio")
+        if element == 67:
+            print("holmio")
+        if element == 68:
+            print("erbio")
+        if element == 69:
+            print("tulio")
+        if element == 70:
+            print("iterbio")
+        if element == 71:
+            print("lutecio")
+        if element == 72:
+            print("hafnio")
+        if element == 73:
+            print("tantalo")
+        if element == 74:
+            print("wolframio")
+        if element == 75:
+            print("renio")
+        if element == 76:
+            print("osmio")
+        if element == 77:
+            print("iridio")
+        if element == 78:
+            print("platino")
+        if element == 79:
+            print("oro")
+        if element == 80:
+            print("mercurio")
+        if element == 81:
+            print("talio")
+        if element == 82:
+            print("plomo")
+        if element == 83:
+            print("bismuto")
+        if element == 84:
+            print("polonio")
+        if element == 85:
+            print("astato")
+        if element == 86:
+            print("radon")
+        if element == 87:
+            print("francio")
+        if element == 88:
+            print("radio")
+        if element == 89:
+            print("actinio")
+        if element == 90:
+            print("torio")
+        if element == 91:
+            print("protactinio")
+        if element == 92:
+            print("uranio")
+        if element == 93:
+            print("neptunio")
+        if element == 94:
+            print("plutonio")
+        if element == 95:
+            print("americio")
+        if element == 96:
+            print("curio")
+        if element == 97:
+            print("berkelio")
+        if element == 98:
+            print("californio")
+        if element == 99:
+            print("einstenio")
+        if element == 100:
+            print("fermio")
+        if element == 101:
+            print("mendelevio")
+        if element == 102:
+            print("nobelio")
+        if element == 103:
+            print("lawrencio")
+        if element == 104:
+            print("rutherfordio")
+        if element == 105:
+            print("dubnio")
+        if element == 106:
+            print("seaborgio")
+        if element == 107:
+            print("bohrio")
+        if element == 108:
+            print("hasio")
+        if element == 109:
+            print("meitnerio")
+        if element == 110:
+            print("darmstatio")
+        if element == 111:
+            print("roentgenio")
+        if element == 112:
+            print("copernicio")
+        if element == 113:
+            print("nihonio")
+        if element == 114:
+            print("flerevio")
+        if element == 115:
+            print("moscovio")
+        if element == 116:
+            print("livermorio")
+        if element == 117:
+            print("teneso")
+        if element == 118:
+            print("organeson")
