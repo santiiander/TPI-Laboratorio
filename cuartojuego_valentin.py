b708e6af0674f8f3f8976479e3e55f99efab6c06
valebraca03 <valebraca03@gmail.com>
2022-11-01 07:21:02 -0300

    puse la parte de atacar y defender pero no me vuelve a preguntar que hacer

--- cuartojuego_valentin.py
+++ cuartojuego_valentin.py
@@ -1,4 +1,5 @@
 def ver_dragon():
+	print(          "                                   HP:", hp, "                              ")
 	print(	        "			⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⡼⠀⢀⡀⣀⢱⡄⡀⠀⠀⠀⢲⣤⣤⣤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀          ")
 	print(		    "			⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⡿⠛⠋⠁⣤⣿⣿⣿⣧⣷⠀⠀⠘⠉⠛⢻⣷⣿⣽⣿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀         ")
 	print(			"			⠀⠀⠀⠀⠀⠀⢀⣴⣞⣽⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠠⣿⣿⡟⢻⣿⣿⣇⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣟⢦⡀⠀⠀⠀⠀⠀⠀        ")
@@ -32,33 +33,121 @@ print()
 if 1==dificultad:
 	d1=print("su dificultad es facil")
 	hp = 1000
+	tuhp=500
+	hpmax=1500
 else:
 		if 2==dificultad:
 		   d2=print("su dificultad es normal")
 		   hp = 5000
+		   tuhp=1000
+		   hpmax=3000
 		else:
 			if 3==dificultad:
 				d3=print("su dificultad es dificil")
 			hp = 10000
-
-print("                                   HP:", hp, "                                ")
+			tuhp=5000
+			hpmax=7000
 print(ver_dragon())
-
-#atacar
 while True:
 	print()
 	print()
-	lansar= int(input("lansar dado con (1)"))
-	if lansar == 1:
-		cifra = randint(1, 2)
-		print(cifra)
-		if cifra == 1:
-			print("no atacas")
-		elif cifra == 2:
-			danio = randint(50,100)
-			hp = hp-danio
-			print("usted hara ", danio)
-			print("                                   HP:",hp, "                        ")
-			print(ver_dragon())
+	accion = int(input("que decea hacer selecione atacar(1) o defender(2)"))
+	if accion == 1:
+		#atacar
+		while True:
+			print()
+			print()
+			lansar= int(input("lansar dado con (1)"))
+			if lansar == 1:
+				cifra = randint(1,12)
+				if cifra == 1:
+					print("no atacas")
+				elif cifra == 2:
+					danio = randint(50,100)
+					hp = hp-danio
+					print("usted hara ", danio)
+					print(ver_dragon())
+				elif cifra == 3:
+					danio=randint(50,100)
+					hp=hp-danio
+					tuhp=tuhp-danio
+					print("usted hara y se hara  ", danio)
+					print(ver_dragon())
+				elif cifra == 4:
+					danio1=randint(50,100)
+					danio2=randint(50,100)
+					danio=danio1+danio2
+					hp=hp-danio
+					print("usted hara ",danio)
+					print(ver_dragon())
+				elif cifra == 5:
+					print("no atacas")
+				elif cifra == 6:
+					d2 = randint(50, 100)
+					danio = d2 * 2
+					hp = hp - danio
+					print("usted hara ", danio)
+					print(ver_dragon())
+				elif cifra == 7:
+					danio=randint(50,100)
+					if danio >= 65 :
+						print("usted hara ", danio)
+						hp=hp-danio
+						print(ver_dragon())
+					else:
+						print("no atacas")
+				elif cifra == 8:
+					d2= randint(50,100)
+					danio=d2*4
+					hp=hp-danio
+					print("usted hara ", danio)
+					print(ver_dragon())
+				elif cifra == 9:
+					danio = randint(100, 200)
+					hp = hp - danio
+					tuhp = tuhp - danio
+					print("usted hara y se hara  ", danio)
+					print(ver_dragon())
+				elif cifra == 10:
+					d1=randint(10,20)
+					danio=d1**2
+					hp=hp-danio
+					print("usted hara ", danio)
+					print(ver_dragon())
+				elif cifra == 11:
+					danio=randint(50,100)
+					if danio <= 65 :
+						print("usted hara ", danio)
+						hp=hp-danio
+						print(ver_dragon())
+					else:
+						print("no atacas")
+				elif cifra == 12:
+					print("no atacas")
 
-	if hp == 0: break
+			if hp <= 0: break
+	elif accion == 2:
+		while True:
+			print()
+			print()
+			lansar = int(input("lansar dado con (1)"))
+			if lansar == 1:
+				cifra = randint(1,5)
+				if cifra == 1:
+					tuhp=tuhp+danio
+					print("usted no recibe danio ")
+				elif cifra == 2:
+					hp = hp
+					print("le regresas el danio")
+				elif cifra == 3:
+					if tuhp >= hpmax:
+						print("usted no puede sumar mas vida")
+					else:
+						print("usted consigue 500 de vida")
+						print(tuhp + 500)
+				elif cifra == 4:
+					hp = hp
+					print("le regresas el danio")
+				elif cifra == 5:
+					tuhp = tuhp + danio
+					print("usted no recibe danio ")
