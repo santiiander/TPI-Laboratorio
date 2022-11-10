e5667ee5690f801b22610452aedab4ac5562fef8
Bettini17 <gonzalobettini39@gmail.com>
2022-11-10 17:03:21 -0300

    juego de Gonzalo 10 preguntas

--- segundojuego_gonzalo.py
+++ segundojuego_gonzalo.py
@@ -17,7 +17,7 @@ if rta == 3:
     print("Su puntaje:",rtaC1,"puntos")
 else:
     print("respuesta incorrecta")
-    rtaC0 = 0
+    rtaC1 = 0
 print("----------segunda pregunta----------")
 print("La Copa del Mundo de 2026 se jugará en tres países diferentes. ¿Puedes elegir la opcion correcta?")
 seleccion1 = str("argentina,brasil y chile")
@@ -31,11 +31,11 @@ print("4", seleccion4)
 rta = int(input("ingrese el numero de la opcion correcta"))
 if rta == 2:
     print("respuesta correcta")
-    rtaC1 = 10
-    print("Su puntaje:",rtaC1,"puntos")
+    rtaC2 = 10
+    print("Su puntaje:",rtaC2,"puntos")
 else:
     print("respuesta incorrecta")
-    rtaC0 = 0
+    rtaC2 = 0
 print("----------tercera pregunta----------")
 print("¿Qué selección llegó a tres finales de la Copa del Mundo pero nunca ganó el título?")
 seleccion1 = str("suiza")
@@ -49,138 +49,138 @@ print("4", seleccion4)
 rta = int(input("ingrese el numero de la opcion correcta"))
 if rta == 4:
     print("respuesta correcta")
-    rtaC1 = 10
-    print("Su puntaje:",rtaC1,"puntos")
+    rtaC3 = 10
+    print("Su puntaje:",rtaC3,"puntos")
 else:
     print("respuesta incorrecta")
-    rtaC0 = 0
-    print("----------cuarta pregunta----------")
-    print("¿Que jugador de futbol es el maximo goleador de este torneo?")
-    seleccion1 = str("Klose")
-    seleccion2 = str("Pelé")
-    seleccion3 = str("Muller")
-    seleccion4 = str("Ronaldo Nazario")
-    print("1", seleccion1)
-    print("2", seleccion2)
-    print("3", seleccion3)
-    print("4", seleccion4)
-    rta = int(input("ingrese el numero de la opcion correcta"))
-    if rta == 1:
-        print("respuesta correcta")
-        rtaC1 = 10
-        print("Su puntaje:", rtaC1, "puntos")
-    else:
+    rtaC3 = 0
+print("----------cuarta pregunta----------")
+print("¿Que jugador de futbol es el maximo goleador de este torneo?")
+seleccion1 = str("Klose")
+seleccion2 = str("Pelé")
+seleccion3 = str("Muller")
+seleccion4 = str("Ronaldo Nazario")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC4 = 10
+    print("Su puntaje:", rtaC4, "puntos")
+else:
         print("respuesta incorrecta")
-        rtaC0 = 0
-        print("----------quinta pregunta----------")
-        print("¿Qué animal fue el escogido para ilustrar la mascota del Mundial de Rusia?")
-        seleccion1 = str("Lobo")
-        seleccion2 = str("Perro")
-        seleccion3 = str("Gato")
-        seleccion4 = str("Loro")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
-        rta = int(input("ingrese el numero de la opcion correcta"))
-        if rta == 1:
-            print("respuesta correcta")
-            rtaC1 = 10
-            print("Su puntaje:", rtaC1, "puntos")
-        else:
-            print("respuesta incorrecta")
-            rtaC0 = 0
-            print("----------sexta pregunta----------")
-            print("¿Recuerdas dónde se jugó la Copa del Mundial del año 2010?")
-            seleccion1 = str("Italia")
-            seleccion2 = str("Sudafrica")
-            seleccion3 = str("Alemania")
-            seleccion4 = str("España")
-            print("1", seleccion1)
-            print("2", seleccion2)
-            print("3", seleccion3)
-            print("4", seleccion4)
-            rta = int(input("ingrese el numero de la opcion correcta"))
-            if rta == 2:
-                print("respuesta correcta")
-                rtaC1 = 10
-                print("Su puntaje:", rtaC1, "puntos")
-            else:
-                print("respuesta incorrecta")
-                rtaC0 = 0
-                print("----------septima pregunta----------")
-                print("“Tenía miedo de que se me cayera el trofeo”. ¿Qué ganador del mundial confesó esto?")
-                seleccion1 = str("iniesta")
-                seleccion2 = str("Puyol")
-                seleccion3 = str("Maradona")
-                seleccion4 = str("Matthaus")
-                print("1", seleccion1)
-                print("2", seleccion2)
-                print("3", seleccion3)
-                print("4", seleccion4)
-                rta = int(input("ingrese el numero de la opcion correcta"))
-                if rta == 4:
-                    print("respuesta correcta")
-                    rtaC1 = 10
-                    print("Su puntaje:", rtaC1, "puntos")
-                else:
-                    print("respuesta incorrecta")
-                    rtaC0 = 0
-                    print("----------octava pregunta----------")
-                    print("¿Cuánto pesa el trofeo de la Copa del Mundo?")
-                    seleccion1 = str("2 kilos")
-                    seleccion2 = str("5 kilos")
-                    seleccion3 = str("6 kilos")
-                    seleccion4 = str("8 kilos")
-                    print("1", seleccion1)
-                    print("2", seleccion2)
-                    print("3", seleccion3)
-                    print("4", seleccion4)
-                    rta = int(input("ingrese el numero de la opcion correcta"))
-                    if rta == 3:
-                        print("respuesta correcta")
-                        rtaC1 = 10
-                        print("Su puntaje:", rtaC1, "puntos")
-                    else:
-                        print("respuesta incorrecta")
-                        rtaC0 = 0
-                        print("----------novena pregunta----------")
-                        print("En el Mundial de México en 1986, el argentino Diego Maradona anotó un gol con la mano, que se convirtió en una leyenda en el fútbol y es conocido como “la mano de Dios”. ¿Contra qué equipo marcó ese famoso tanto?")
-                        seleccion1 = str("Francia")
-                        seleccion2 = str("Brasil")
-                        seleccion3 = str("Alemania")
-                        seleccion4 = str("Inglaterra")
-                        print("1", seleccion1)
-                        print("2", seleccion2)
-                        print("3", seleccion3)
-                        print("4", seleccion4)
-                        rta = int(input("ingrese el numero de la opcion correcta"))
-                        if rta == 4:
-                            print("respuesta correcta")
-                            rtaC1 = 10
-                            print("Su puntaje:", rtaC1, "puntos")
-                        else:
-                            print("respuesta incorrecta")
-                            rtaC0 = 0
-                            print("----------decima pregunta----------")
-                            print("¿Dónde se disputó la Copa Mundial de 1998?")
-                            seleccion1 = str("Italia")
-                            seleccion2 = str("Francia")
-                            seleccion3 = str("Alemania")
-                            seleccion4 = str("Japon")
-                            print("1", seleccion1)
-                            print("2", seleccion2)
-                            print("3", seleccion3)
-                            print("4", seleccion4)
-                            rta = int(input("ingrese el numero de la opcion correcta"))
-                            if rta == 2:
-                                print("respuesta correcta")
-                                rtaC1 = 10
-                                print("Su puntaje:", rtaC1, "puntos")
-                            else:
-                                print("respuesta incorrecta")
-                                rtaC0 = 0
-rtaT = (rtaC1*10)
+        rtaC4 = 0
+print("----------quinta pregunta----------")
+print("¿Qué animal fue el escogido para ilustrar la mascota del Mundial de Rusia?")
+seleccion1 = str("Lobo")
+seleccion2 = str("Perro")
+seleccion3 = str("Gato")
+seleccion4 = str("Loro")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC5 = 10
+    print("Su puntaje:", rtaC5, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC5 = 0
+print("----------sexta pregunta----------")
+print("¿Recuerdas dónde se jugó la Copa del Mundial del año 2010?")
+seleccion1 = str("Italia")
+seleccion2 = str("Sudafrica")
+seleccion3 = str("Alemania")
+seleccion4 = str("España")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 2:
+    print("respuesta correcta")
+    rtaC6 = 10
+    print("Su puntaje:", rtaC6, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC6 = 0
+print("----------septima pregunta----------")
+print("“Tenía miedo de que se me cayera el trofeo”. ¿Qué ganador del mundial confesó esto?")
+seleccion1 = str("iniesta")
+seleccion2 = str("Puyol")
+seleccion3 = str("Maradona")
+seleccion4 = str("Matthaus")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 4:
+    print("respuesta correcta")
+    rtaC7 = 10
+    print("Su puntaje:", rtaC7, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC7 = 0
+print("----------octava pregunta----------")
+print("¿Cuánto pesa el trofeo de la Copa del Mundo?")
+seleccion1 = str("2 kilos")
+seleccion2 = str("5 kilos")
+seleccion3 = str("6 kilos")
+seleccion4 = str("8 kilos")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 3:
+    print("respuesta correcta")
+    rtaC8 = 10
+    print("Su puntaje:", rtaC8, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC8 = 0
+print("----------novena pregunta----------")
+print("En el Mundial de México en 1986, el argentino Diego Maradona anotó un gol con la mano, que se convirtió en una leyenda en el fútbol y es conocido como “la mano de Dios”. ¿Contra qué equipo marcó ese famoso tanto?")
+seleccion1 = str("Francia")
+seleccion2 = str("Brasil")
+seleccion3 = str("Alemania")
+seleccion4 = str("Inglaterra")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 4:
+    print("respuesta correcta")
+    rtaC9 = 10
+    print("Su puntaje:", rtaC9, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC9 = 0
+print("----------decima pregunta----------")
+print("¿Dónde se disputó la Copa Mundial de 1998?")
+seleccion1 = str("Italia")
+seleccion2 = str("Francia")
+seleccion3 = str("Alemania")
+seleccion4 = str("Japon")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 2:
+    print("respuesta correcta")
+    rtaC10 = 10
+    print("Su puntaje:", rtaC10, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC10 = 0
+rtaT = (rtaC1+rtaC2+rtaC3+rtaC4+rtaC5+rtaC6+rtaC7+rtaC8+rtaC9+rtaC10)
 print("hemos finalizado la trivia y su puntaje es de:" ,rtaT, "puntos")
 if rtaT == 100:
     print("llegaste al puntaje maximo, felicitaciones")
