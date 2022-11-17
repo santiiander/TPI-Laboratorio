11f131728373c2a5bebf73967f6d36cf8cb0cea5
Bettini17 <gonzalobettini39@gmail.com>
2022-11-17 09:15:11 -0300

    juego de Gonzalo 22 preguntas

--- segundojuego_gonzalo.py
+++ segundojuego_gonzalo.py
@@ -1,7 +1,7 @@
 print("Bienvenidos a Trivia Sports Mundial")
 print("¿Capaz de lograr el maximo puntaje? Cada respuesta correcta son 10 puntos...Comenzemos")
 print("----------primera pregunta----------")
-print("¿Que seleccion de futbol gano el primer mundial en 1930?")
+print("¿Que seleccion de futbol gano el primer mundial?")
 seleccion1 = str("argentina")
 seleccion2 = str("alemania")
 seleccion3 = str("uruguay")
@@ -180,10 +180,225 @@ if rta == 2:
 else:
     print("respuesta incorrecta")
     rtaC10 = 0
-rtaT = (rtaC1+rtaC2+rtaC3+rtaC4+rtaC5+rtaC6+rtaC7+rtaC8+rtaC9+rtaC10)
+print("----------decima primer pregunta----------")
+print("Cuantas selecciones participan en un mundial de futbol?")
+seleccion1 = str("32 equipos")
+seleccion2 = str("36 equipos")
+seleccion3 = str("40 equipos")
+seleccion4 = str("42 equipos")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC11 = 10
+    print("Su puntaje:", rtaC11, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC11 = 0
+print("----------decima segunda pregunta----------")
+print("Que país ha participado mas veces en la copa del mundo?")
+seleccion1 = str("Argentina")
+seleccion2 = str("Brasil")
+seleccion3 = str("Alemania")
+seleccion4 = str("Francia")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 2:
+    print("respuesta correcta")
+    rtaC12 = 10
+    print("Su puntaje:", rtaC12, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC12 = 0
+print("----------decima tercer pregunta----------")
+print("Donde se disputo la copa del mundo 2006")
+seleccion1 = str("España")
+seleccion2 = str("Sudafrica")
+seleccion3 = str("Alemania")
+seleccion4 = str("Japon/Corea del sur")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 3:
+    print("respuesta correcta")
+    rtaC13 = 10
+    print("Su puntaje:", rtaC13, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC13 = 0
+print("----------decima cuarta pregunta----------")
+print("En que año se jugo la primera copa del  mundo?")
+seleccion1 = str("1938")
+seleccion2 = str("1926")
+seleccion3 = str("1942")
+seleccion4 = str("1930")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 4:
+    print("respuesta correcta")
+    rtaC14 = 10
+    print("Su puntaje:", rtaC14, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC14 = 0
+print("----------decima quinta pregunta----------")
+print("Cual selección es la que mas ha ganado la copa?")
+seleccion1 = str("Italia")
+seleccion2 = str("Francia")
+seleccion3 = str("Brasil")
+seleccion4 = str("Alemania")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 3:
+    print("respuesta correcta")
+    rtaC15 = 10
+    print("Su puntaje:", rtaC15, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC15 = 0
+print("----------decima sexta pregunta----------")
+print("Cada cuantos años se realiza esta competición?")
+seleccion1 = str("Cada 4 años")
+seleccion2 = str("Cada 6 años")
+seleccion3 = str("Cada 5 años")
+seleccion4 = str("Cada 3 años")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC16 = 10
+    print("Su puntaje:", rtaC16, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC16 = 0
+print("----------decima septima pregunta----------")
+print("Que selección ha disputado mas finales de esta competencia?")
+seleccion1 = str("Holanda")
+seleccion2 = str("Inglaterra")
+seleccion3 = str("Alemania")
+seleccion4 = str("Brasil")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 3:
+    print("respuesta correcta")
+    rtaC17 = 10
+    print("Su puntaje:", rtaC17, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC17 = 0
+print("----------decima octava pregunta----------")
+print("Quien es el único jugador en obtener tres veces la copa del mundo?")
+seleccion1 = str("Pelé")
+seleccion2 = str("Cafú")
+seleccion3 = str("Meazza")
+seleccion4 = str("Maradona")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC18 = 10
+    print("Su puntaje:", rtaC18, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC18 = 0
+print("----------decima novena pregunta----------")
+print("Quien fue el jugador mas joven en ganar la copa del mundo?")
+seleccion1 = str("Mbappe")
+seleccion2 = str("Pelé")
+seleccion3 = str("Whiteside")
+seleccion4 = str("Opabunmi")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 2:
+    print("respuesta correcta")
+    rtaC19 = 10
+    print("Su puntaje:", rtaC19, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC19 = 0
+print("----------vigésima pregunta----------")
+print("Quien fue el jugador mas veterano en disputar una copa del mundo?")
+seleccion1 = str("Peter Cech")
+seleccion2 = str("Dino Soff")
+seleccion3 = str("Essam-El-Hadary")
+seleccion4 = str("Macdonald Taylor")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 3:
+    print("respuesta correcta")
+    rtaC20 = 10
+    print("Su puntaje:", rtaC20, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC20 = 0
+print("----------vigésima primer pregunta----------")
+print("Cual de estas selecciones nunca disputador un mundial?")
+seleccion1 = str("Finlandia")
+seleccion2 = str("Israel")
+seleccion3 = str("El Salvador")
+seleccion4 = str("Nueva Zelanda")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC21 = 10
+    print("Su puntaje:", rtaC21, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC21 = 0
+print("----------vigésima segunda pregunta----------")
+print("Quien anoto el primer gol en la historia de los mundiales?")
+seleccion1 = str("Laurent")
+seleccion2 = str("Delfour")
+seleccion3 = str("Petrone")
+seleccion4 = str("Chividini")
+print("1", seleccion1)
+print("2", seleccion2)
+print("3", seleccion3)
+print("4", seleccion4)
+rta = int(input("ingrese el numero de la opcion correcta"))
+if rta == 1:
+    print("respuesta correcta")
+    rtaC22 = 10
+    print("Su puntaje:", rtaC22, "puntos")
+else:
+    print("respuesta incorrecta")
+    rtaC22 = 0
+rtaT = (rtaC1+rtaC2+rtaC3+rtaC4+rtaC5+rtaC6+rtaC7+rtaC8+rtaC9+rtaC10+rtaC11+rtaC12+rtaC13+rtaC14+rtaC15+rtaC16+rtaC17+rtaC18+rtaC19+rtaC20+rtaC21+rtaC22)
 print("hemos finalizado la trivia y su puntaje es de:" ,rtaT, "puntos")
-if rtaT == 100:
+if rtaT == 220:
     print("llegaste al puntaje maximo, felicitaciones")
-
 
 
