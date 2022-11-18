0c153534c2da6c561431ff56dc23c23b4d0b24d7
LeonCamandona <camandonaleon@gmail.com>
2022-11-18 13:47:04 -0300

    actualizacion

--- segundojuego_gonzalo.py
+++ segundojuego_gonzalo.py
@@ -139,6 +139,7 @@ while menu==2:
         print("2) croacia")
         print("3) hungria")
         print("4) holanda")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 4:
             print("respuesta correcta")
@@ -149,14 +150,11 @@ while menu==2:
             rtaC2 = 0
         print("----------tercer pregunta----------")
         print("¿Que jugador de futbol es el maximo goleador de este torneo?")
-        seleccion1 = str("Klose")
-        seleccion2 = str("Pelé")
-        seleccion3 = str("Muller")
-        seleccion4 = str("Ronaldo Nazario")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Klose")
+        print("2) Pelé")
+        print("3) Muller")
+        print("4) Ronaldo Nazario")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 1:
             print("respuesta correcta")
@@ -167,14 +165,11 @@ while menu==2:
             rtaC3 = 0
         print("----------cuarta pregunta----------")
         print("“Tenía miedo de que se me cayera el trofeo”. ¿Qué ganador del mundial confesó esto?")
-        seleccion1 = str("Iniesta")
-        seleccion2 = str("Puyol")
-        seleccion3 = str("Maradona")
-        seleccion4 = str("Matthaus")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Iniesta")
+        print("2) Puyol")
+        print("3) Maradona")
+        print("4) Matthaus")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 4:
             print("respuesta correcta")
@@ -185,14 +180,11 @@ while menu==2:
             rtaC4 = 0
         print("----------quinta pregunta----------")
         print("¿Cuánto pesa el trofeo de la Copa del Mundo?")
-        seleccion1 = str("2 kilos")
-        seleccion2 = str("5 kilos")
-        seleccion3 = str("6 kilos")
-        seleccion4 = str("8 kilos")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) 2 kilos")
+        print("2) 5 kilos")
+        print("3) 6 kilos")
+        print("4) 8 kilos")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 3:
             print("respuesta correcta")
@@ -203,14 +195,11 @@ while menu==2:
             rtaC5 = 0
         print("----------sexta pregunta----------")
         print("En el Mundial de México en 1986, el argentino Diego Maradona anotó un gol con la mano, que se convirtió en una leyenda en el fútbol y es conocido como “la mano de Dios”. ¿Contra qué equipo marcó ese famoso tanto?")
-        seleccion1 = str("Francia")
-        seleccion2 = str("Brasil")
-        seleccion3 = str("Alemania")
-        seleccion4 = str("Inglaterra")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Francia")
+        print("2) Brasil")
+        print("3) Alemania")
+        print("4) Inglaterra")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 4:
             print("respuesta correcta")
@@ -221,14 +210,11 @@ while menu==2:
             rtaC6 = 0
         print("----------septima y ultima pregunta----------")
         print("¿Dónde se disputó la Copa Mundial de 1998?")
-        seleccion1 = str("Italia")
-        seleccion2 = str("Francia")
-        seleccion3 = str("Alemania")
-        seleccion4 = str("Japon")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Italia")
+        print("2) Francia")
+        print("3) Alemania")
+        print("4) Japon")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 2:
             print("respuesta correcta")
@@ -245,14 +231,11 @@ while menu==2:
     while dificultadjuego2 == 3:
         print("----------primera pregunta----------")
         print("Que país ha participado mas veces en la copa del mundo?")
-        seleccion1 = str("Argentina")
-        seleccion2 = str("Brasil")
-        seleccion3 = str("Alemania")
-        seleccion4 = str("Francia")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Argentina")
+        print("2) Brasil")
+        print("3) Alemania")
+        print("4) Francia")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 2:
             print("respuesta correcta")
@@ -263,14 +246,11 @@ while menu==2:
             rtaC1 = 0
         print("----------segunda pregunta----------")
         print("¿En que año se jugo la primera copa del  mundo?")
-        seleccion1 = str("1938")
-        seleccion2 = str("1926")
-        seleccion3 = str("1942")
-        seleccion4 = str("1930")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) 1938")
+        print("2) 1926")
+        print("3) 1942")
+        print("4) 1930")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 4:
             print("respuesta correcta")
@@ -281,14 +261,11 @@ while menu==2:
             rtaC2 = 0
         print("----------tercer pregunta----------")
         print("Que selección ha disputado mas finales de esta competencia?")
-        seleccion1 = str("Holanda")
-        seleccion2 = str("Inglaterra")
-        seleccion3 = str("Alemania")
-        seleccion4 = str("Brasil")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Holanda")
+        print("2) Inglaterra")
+        print("3) Alemania")
+        print("4) Brasil")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 3:
             print("respuesta correcta")
@@ -299,14 +276,11 @@ while menu==2:
             rtaC3 = 0
         print("----------cuarta pregunta----------")
         print("Quien es el único jugador en obtener tres veces la copa del mundo?")
-        seleccion1 = str("Pelé")
-        seleccion2 = str("Cafú")
-        seleccion3 = str("Meazza")
-        seleccion4 = str("Maradona")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Pelé")
+        print("2) Cafú")
+        print("3) Meazza")
+        print("4) Maradona")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 1:
             print("respuesta correcta")
@@ -317,14 +291,11 @@ while menu==2:
             rtaC4 = 0
         print("----------quinta pregunta----------")
         print("Quien fue el jugador mas joven en ganar la copa del mundo?")
-        seleccion1 = str("Mbappe")
-        seleccion2 = str("Pelé")
-        seleccion3 = str("Whiteside")
-        seleccion4 = str("Opabunmi")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Mbappe")
+        print("2) Pelé")
+        print("3) Whiteside")
+        print("4) Opabunmi")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 2:
             print("respuesta correcta")
@@ -335,14 +306,11 @@ while menu==2:
             rtaC5 = 0
         print("----------sexta pregunta----------")
         print("Quien fue el jugador mas veterano en disputar una copa del mundo?")
-        seleccion1 = str("Peter Cech")
-        seleccion2 = str("Dino Soff")
-        seleccion3 = str("Essam-El-Hadary")
-        seleccion4 = str("Macdonald Taylor")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Peter Cech")
+        print("2) Dino Soff")
+        print("3) Essam-El-Hadary")
+        print("4) Macdonald Taylor")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 3:
             print("respuesta correcta")
@@ -353,14 +321,11 @@ while menu==2:
             rtaC6 = 0
         print("----------septima pregunta----------")
         print("Cual de estas selecciones nunca disputador un mundial?")
-        seleccion1 = str("Finlandia")
-        seleccion2 = str("Israel")
-        seleccion3 = str("El Salvador")
-        seleccion4 = str("Nueva Zelanda")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Finlandia")
+        print("2) Israel")
+        print("3) El Salvador")
+        print("4) Nueva Zelanda")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 1:
             print("respuesta correcta")
@@ -371,14 +336,11 @@ while menu==2:
             rtaC7 = 0
         print("----------octava y ultima pregunta----------")
         print("¿Quien anoto el primer gol en la historia de los mundiales?")
-        seleccion1 = str("Laurent")
-        seleccion2 = str("Delfour")
-        seleccion3 = str("Petrone")
-        seleccion4 = str("Chividini")
-        print("1", seleccion1)
-        print("2", seleccion2)
-        print("3", seleccion3)
-        print("4", seleccion4)
+        print("1) Laurent")
+        print("2) Delfour")
+        print("3) Petrone")
+        print("4) Chividini")
+
         rta = int(input("ingrese el numero de la opcion correcta"))
         if rta == 1:
             print("respuesta correcta")
@@ -392,9 +354,3 @@ while menu==2:
         print("hemos finalizado la trivia y su puntaje es de:", rtaT, "puntos")
         if rtaT == 70:
             print("llegaste al puntaje maximo, felicitaciones")
-
-
-
-
-
-
