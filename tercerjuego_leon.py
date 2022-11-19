1d24ce48d98e2520a5f42a23fe8fd8fe6ca99b46
LeonCamandona <camandonaleon@gmail.com>
2022-11-19 15:54:04 -0300

    finalizado

--- tercerjuego_leon.py
+++ tercerjuego_leon.py
@@ -4,6 +4,7 @@ while menu == 3:
     def nometal(): print("es un no metal")
     def metal(): print("es un metal")
     def semimetal(): print("es un semi metal")
+    def desconocido(): print("su capacidad de conduccion electrica es aun desconocida")
     def intentos(atomo):
         vidas=3
         guess = str.lower(input())
@@ -36,7 +37,6 @@ while menu == 3:
     print("usted debera adivinar de que elemento se trata usando los datos que se iran otorgando por cada intento hasta un maximo de 3")
     vidas=3
     element = random.randint(1, 118)
-    print(element)
     if element == 1:
         atomo="hidrogeno"
         nometal(),intentos(atomo)
@@ -45,13 +45,13 @@ while menu == 3:
         nometal(),intentos(atomo)
     if element == 3:
         atomo = "litio"
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 4:
         atomo = "berilio"
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 5:
         atomo = "boro"
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 6:
         atomo = "carbono"
         nometal(), intentos(atomo)
@@ -69,16 +69,16 @@ while menu == 3:
         nometal(), intentos(atomo)
     if element == 11:
         atomo = "sodio"
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 12:
         atomo = "magnesio"
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 13:
         atomo = "aluminio"
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 14:
         atomo = ("silicio")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 15:
         atomo = ("fosforo")
         nometal(), intentos(atomo)
@@ -93,49 +93,49 @@ while menu == 3:
         nometal(), intentos(atomo)
     if element == 19:
         atomo = ("potasio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 20:
         atomo = ("calcio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 21:
         atomo = ("escandio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 22:
         atomo = ("titanio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 23:
         atomo = ("vanadio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 24:
         atomo = ("cromo")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 25:
         atomo = ("manganeso")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 26:
         atomo = ("hierro")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 27:
         atomo = ("cobalto")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 28:
         atomo = ("niquel")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 29:
         atomo = ("cobre")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 30:
-        atomo = ("zinc")
+        metal = ("zinc")
         nometal(), intentos(atomo)
     if element == 31:
         atomo = ("galio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 32:
         atomo = ("germanio")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 33:
         atomo = ("arsenico")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 34:
         atomo = ("selenio")
         nometal(), intentos(atomo)
@@ -147,52 +147,52 @@ while menu == 3:
         nometal(), intentos(atomo)
     if element == 37:
         atomo = ("rubidio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 38:
         atomo = ("estroncio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 39:
         atomo = ("itrio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 40:
         atomo = ("circonio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 41:
         atomo = ("niobio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 42:
         atomo = ("molibdeno")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 43:
         atomo = ("tecnecio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 44:
         atomo = ("rutenio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 45:
         atomo = ("rodio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 46:
         atomo = ("paladio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 47:
         atomo = ("plata")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 48:
         atomo = ("cadmio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 49:
         atomo = ("indio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 50:
         atomo = ("estaño")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 51:
         atomo = ("antimonio")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 52:
         atomo = ("telurio")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 53:
         atomo = ("yodo")
         nometal(), intentos(atomo)
@@ -201,193 +201,193 @@ while menu == 3:
         nometal(), intentos(atomo)
     if element == 55:
         atomo = ("cesio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 56:
         atomo = ("bario")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 57:
         atomo = ("lantano")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 58:
         atomo = ("cerio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 59:
         atomo = ("praseodimio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 60:
         atomo = ("neodimio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 61:
         atomo = ("prometio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 62:
         atomo = ("samario")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 63:
         atomo = ("europio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 64:
         atomo = ("gadolinio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 65:
         atomo = ("terbio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 66:
         atomo = ("disprosio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 67:
         atomo = ("holmio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 68:
         atomo = ("erbio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 69:
         atomo = ("tulio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 70:
         atomo = ("iterbio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 71:
         atomo = ("lutecio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 72:
         atomo = ("hafnio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 73:
         atomo = ("tantalo")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 74:
         atomo = ("wolframio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 75:
         atomo = ("renio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 76:
         atomo = ("osmio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 77:
         atomo = ("iridio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 78:
         atomo = ("platino")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 79:
         atomo = ("oro")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 80:
         atomo = ("mercurio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 81:
         atomo = ("talio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 82:
         atomo = ("plomo")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 83:
         atomo = ("bismuto")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 84:
         atomo = ("polonio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 85:
         atomo = ("astato")
-        nometal(), intentos(atomo)
+        semimetal(), intentos(atomo)
     if element == 86:
         atomo = ("radon")
         nometal(), intentos(atomo)
     if element == 87:
         atomo = ("francio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 88:
         atomo = ("radio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 89:
         atomo = ("actinio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 90:
         atomo = ("torio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 91:
         atomo = ("protactinio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 92:
         atomo = ("uranio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 93:
         atomo = ("neptunio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 94:
         atomo = ("plutonio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 95:
         atomo = ("americio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 96:
         atomo = ("curio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 97:
         atomo = ("berkelio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 98:
         atomo = ("californio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 99:
         atomo = ("einstenio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 100:
         atomo = ("fermio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 101:
         atomo = ("mendelevio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 102:
         atomo = ("nobelio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 103:
         atomo = ("lawrencio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 104:
         atomo = ("rutherfordio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 105:
         atomo = ("dubnio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 106:
         atomo = ("seaborgio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 107:
         atomo = ("bohrio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 108:
         atomo = ("hasio")
-        nometal(), intentos(atomo)
+        metal(), intentos(atomo)
     if element == 109:
         atomo = ("meitnerio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 110:
         atomo = ("darmstatio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 111:
         atomo = ("roentgenio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 112:
         atomo = ("copernicio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 113:
         atomo = ("nihonio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 114:
         atomo = ("flerevio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 115:
         atomo = ("moscovio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 116:
         atomo = ("livermorio")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 117:
         atomo = ("teneso")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
     if element == 118:
         atomo = ("organeson")
-        nometal(), intentos(atomo)
+        desconocido(), intentos(atomo)
